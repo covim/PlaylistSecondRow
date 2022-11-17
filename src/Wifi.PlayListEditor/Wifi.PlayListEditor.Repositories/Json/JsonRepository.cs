2f015a946244a02dedfcd45969edd3a6e37303cb
Matthias Covi (Wifi Festplatte) <m.covi@cwp.uno>
2022-11-17 21:27:32 +0100

    Json Implementation

---
+++ src/Wifi.PlayListEditor/Wifi.PlayListEditor.Repositories/Json/JsonRepository.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wifi.PlayListEditor.Types;
+
+namespace Wifi.PlayListEditor.Repositories.Json
+{
+    public class JsonRepository : IRepository
+    {
+        public string Extension => ".json";
+
+        public string Description => "Wifi playlist format";
+
+        public IPlaylist Load(string playlistFilePath)
+        {
+            throw new NotImplementedException();
+            //todo string einlesen und deserialisiern
+        }
+
+        public void Save(IPlaylist playlist, string playlistFilePath)
+        {
+            var entity = playlist.ToEntity();
+
+            string json = JsonConvert.SerializeObject(entity);
+
+            //todo string speichern
+
+        }
+    }
+}
---
+++ src/Wifi.PlayListEditor/Wifi.PlayListEditor.Repositories/Json/PlaylistEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wifi.PlayListEditor.Repositories.Json
+{
+    internal class PlaylistEntity
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public string createdAt { get; set; }
+        public IEnumerable<ItemEntity> items { get; set; }
+    }
+}

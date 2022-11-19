6da62f4a43b8f8f7acc514ed580feebc8d5d0e1f
Matthias Covi (Wifi Festplatte) <m.covi@cwp.uno>
2022-11-19 22:14:57 +0100

    Save und SaveTest

--- src/Wifi.PlayListEditor/Wifi.PlayListEditor.Repositories/Json/JsonRepository.cs
+++ src/Wifi.PlayListEditor/Wifi.PlayListEditor.Repositories/Json/JsonRepository.cs
@@ -1,18 +1,39 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO.Abstractions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Wifi.PlayListEditor.Types;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Wifi.PlayListEditor.Repositories.Json
 {
     public class JsonRepository : IRepository
     {
-        public string Extension => ".json";
+        private readonly string _extension;
+        private readonly IFileSystem _fileSystem;
+        private readonly IPlaylistItemFactory _playlistItemFactory;
 
-        public string Description => "Wifi playlist format";
+        public JsonRepository(IFileSystem fileSystem, IPlaylistItemFactory playlistItemFactory)
+        {
+            _fileSystem = fileSystem;
+            _playlistItemFactory = playlistItemFactory;
+            _extension = ".json";
+        }
+        public JsonRepository(IPlaylistItemFactory playlistItemFactory) : this(new FileSystem(), playlistItemFactory)
+        {
+            _extension = ".json";
+            _playlistItemFactory = playlistItemFactory;
+        }
+        public string Description => "json Playlist file";
+        public string Extension
+        {
+            get { return _extension; }
+            //set { _extension = value; }
+        }
+
 
         public IPlaylist Load(string playlistFilePath)
         {
@@ -22,11 +43,26 @@ namespace Wifi.PlayListEditor.Repositories.Json
 
         public void Save(IPlaylist playlist, string playlistFilePath)
         {
+            if (playlist == null || string.IsNullOrEmpty(playlistFilePath))
+            {
+                return;
+            }
             var entity = playlist.ToEntity();
 
-            string json = JsonConvert.SerializeObject(entity);
+            var settings = new JsonSerializerSettings();
+            settings.StringEscapeHandling = StringEscapeHandling.EscapeHtml;
 
-            //todo string speichern
+            string json = JsonConvert.SerializeObject(entity);
+                        
+
+            try
+            {
+                _fileSystem.File.WriteAllText(playlistFilePath, json);
+            }
+            catch (Exception)
+            {
+                return;
+            }
 
         }
     }

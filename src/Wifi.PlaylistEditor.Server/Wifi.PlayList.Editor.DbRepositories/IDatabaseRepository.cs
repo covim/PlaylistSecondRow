d45574604254919fb7961713fbbe89d41a0be6d4
Matthias Covi (Wifi Festplatte) <m.covi@cwp.uno>
2022-12-01 21:06:53 +0100

    MongoDBrepository ausgefüllt

---
+++ src/Wifi.PlaylistEditor.Server/Wifi.PlayList.Editor.DbRepositories/IDatabaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using Wifi.PlayList.Editor.DbRepositories.MongoDbEntities;
+
+namespace Wifi.PlayList.Editor.DbRepositories
+{
+    public interface IDatabaseRepository<T>
+    {
+        /// <summary>
+        /// Defines CRUD (Create, Read, Update, Delete) methods for a Database repository
+        /// </summary>
+        
+        Task<List<T>> GetAsync();
+
+        Task<T> GetAsync(string id);
+
+        Task CreateAsync(T newPlaylist);
+
+        Task UpdateAsync(string id, T updatedPlayist);
+
+        Task RemoveAsync(string id);
+
+
+    }
+}
--- src/Wifi.PlaylistEditor.Server/Wifi.PlayList.Editor.DbRepositories/MongoDbRepository.cs
+++ src/Wifi.PlaylistEditor.Server/Wifi.PlayList.Editor.DbRepositories/MongoDbRepository.cs
@@ -1,7 +1,66 @@
-﻿namespace Wifi.PlayList.Editor.DbRepositories
-{
-    public class MongoDbRepository
-    {
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Wifi.PlayList.Editor.DbRepositories.MongoDbEntities;
 
+namespace Wifi.PlayList.Editor.DbRepositories
+{
+    public class MongoDbRepository : IDatabaseRepository<PlaylistEntity>
+    {
+        private IMongoCollection<PlaylistEntity> _playlistsCollection;
+
+        public MongoDbRepository(IOptions<PlaylistDbSettings> playlistDbSettings)
+        {
+            if (playlistDbSettings == null)
+            {
+                return;
+            }
+
+            var mongoClient = new MongoClient(playlistDbSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(playlistDbSettings.Value.DatabaseName);
+
+            _playlistsCollection = mongoDatabase.GetCollection<PlaylistEntity>(playlistDbSettings.Value.CollectionName);
+        }
+
+        public async Task CreateAsync(PlaylistEntity newPlaylist)
+        {
+            if (newPlaylist == null)
+            {
+                return;
+            }
+            await _playlistsCollection.InsertOneAsync(newPlaylist);
+        }
+
+        public async Task<List<PlaylistEntity>> GetAsync()
+        {
+            return await _playlistsCollection.Find(x => true).ToListAsync();
+        }
+
+        public async Task<PlaylistEntity> GetAsync(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return null;
+            }
+            return await _playlistsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task RemoveAsync(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return;
+            }
+            await _playlistsCollection.DeleteOneAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(string id, PlaylistEntity updatedPlaylist)
+        {
+            if (string.IsNullOrEmpty(id) || updatedPlaylist == null)
+            {
+                return;
+            }
+            await _playlistsCollection.ReplaceOneAsync(x => x.Id == id, updatedPlaylist);
+        }
     }
 }

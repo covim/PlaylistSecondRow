e63b0b133de0af08156dc00b4766c3c2dc77d919
Matthias Covi (Wifi Festplatte) <m.covi@cwp.uno>
2022-12-01 21:24:14 +0100

    erster test umgesetzt

---
+++ src/Wifi.PlaylistEditor.Server/Wifi.PlayList.Editor.DbRepositories.Test/MongoDbRepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using Wifi.PlayList.Editor.DbRepositories.MongoDbEntities;
+
+
+namespace Wifi.PlayList.Editor.DbRepositories.Test
+{
+    [TestFixture]
+    public class MongoDbRepositoryTests
+    {
+        private IDatabaseRepository<PlaylistEntity> _fixture;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = Options.Create(new PlaylistDbSettings
+            {
+                ConnectionString = "mongodb://admin:password@localhost:27017",
+                DatabaseName = "playlistDb",
+                CollectionName = "playlists"
+            });
+
+            _fixture = new MongoDbRepository(options);
+        }
+
+        [Test]
+        public async Task CreateAsync()
+        {
+            //arrange
+            var entity = new PlaylistEntity
+            {
+                Author = "DJ Gustl",
+                Title = "Superhits",
+                CreatedAt = "20221201",
+                Id = Guid.NewGuid().ToString(),
+                Items = new List<PlaylistItemEntity>
+                {
+                    new PlaylistItemEntity
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Path = @"/app/uploads/superSong1.mp3"
+                    },
+                    new PlaylistItemEntity
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Path = @"/app/uploads/superSong2.mp3"
+                    }
+                }
+            };
+
+            //act
+            await _fixture.CreateAsync(entity);
+        }
+    }
+}

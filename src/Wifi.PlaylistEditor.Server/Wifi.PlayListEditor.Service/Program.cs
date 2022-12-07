ba0215740b5dda0579388af16bedc49745f05133
Matthias Covi (Wifi Festplatte) <m.covi@cwp.uno>
2022-12-07 19:43:02 +0100

    REST die zweite

--- src/Wifi.PlaylistEditor.Server/Wifi.PlayListEditor.Service/Program.cs
+++ src/Wifi.PlaylistEditor.Server/Wifi.PlayListEditor.Service/Program.cs
@@ -1,13 +1,19 @@
+using Wifi.PlayList.Editor.DbRepositories.MongoDbEntities;
+using Wifi.PlayList.Editor.DbRepositories;
 using Wifi.PlaylistEditor.Factories;
 using Wifi.PlaylistEditor.Types;
 using Wifi.PlayListEditor.Service.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<PlaylistDbSettings>(
+    builder.Configuration.GetSection("PlaylistDbSettings"));
+
 // Add services to the container.
 builder.Services.AddScoped<IPlaylistFactory, PlaylistFactory>();
 builder.Services.AddScoped<IPlaylistService, PlaylistService>();
 builder.Services.AddScoped<IPlaylistItemFactory, PlaylistItemFactory>();
+builder.Services.AddScoped<IDatabaseRepository<PlaylistEntity>, MongoDbRepository>();
 
 builder.Services.AddControllers();
 
@@ -20,3 +26,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
---
+++ src/services/Readme.txt
@@ -0,0 +1,4 @@
+docker compose ausführen:
+
+in ordner in der das .yaml file liegt in der powerShell: docker-compose -f .\docker-compose.yaml up -d 
+

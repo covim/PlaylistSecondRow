4d4bb4556c06e98a57cbf5dfa0587c9887d28da1
Matthias Covi (Wifi Festplatte) <m.covi@cwp.uno>
2022-12-26 18:55:22 +0100

    Added LiteDb

---
+++ src/Database/Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Database;
+using LiteDB;
+using System;
+
+namespace Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+            var newDb = new InitiateDb();
+
+        }
+
+        
+    }
+}
